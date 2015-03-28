7d34c9071fe4bb9369dce60a2f5bd803c259e532
Lucas Wojciechowski <lucas@lucaswoj.com>
2015-03-28 15:44:09 -0400

    Set up build in NetBeans

--- classifier/org/braintrust/InputProcessor.java
+++ classifier/org/braintrust/InputProcessor.java
@@ -1,15 +1,10 @@
 package org.braintrust;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 
 public class InputProcessor {
 
