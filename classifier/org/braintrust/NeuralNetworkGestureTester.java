62fe7c5d7237dc84e9e9ee2e55ba7b4b77c98d70
Ariel Weingarten <asweinga@uwaterloo.ca>
2015-03-28 19:16:09 -0400

    Fix error in GestureTester

--- classifier/org/braintrust/NeuralNetworkGestureTester.java
+++ classifier/org/braintrust/NeuralNetworkGestureTester.java
@@ -10,11 +10,11 @@ public class NeuralNetworkGestureTester {
             NeuralNetwork n = new NeuralNetwork(0.5, new int[]{ 1350, 10, 5, 1 });
 
             for (int i = 0; i < 10; i++) {
-                Tuple<double[], Integer> data = gestureStore.getRandom();
+                Tuple<double[], Integer> data = gestureStore.getRandom(GestureStore.Gesture.CIRCLE);
                 double[] input = data.x;
                 n.train(data.x, new double[]{1});
             }
-            Tuple<double[], Integer> data = gestureStore.getRandom();
+            Tuple<double[], Integer> data = gestureStore.getRandom(GestureStore.Gesture.CIRCLE);
             double result = n.classify(data.x)[0];
             System.out.println("result: " + result);
 	}
